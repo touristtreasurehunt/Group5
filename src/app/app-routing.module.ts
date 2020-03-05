f19b0ec53c54aac1b92050da2ab71d552ad2a86e
JoelLF <rlflaspalmas@hotmail.com>
2020-03-05 08:21:24 +0000

    Added files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },  { path: 'main', loadChildren: './main/main.module#MainPageModule' }
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {}
+
+export class Tab1Page {
+
+  constructor(private router: Router) { }
+
+  goToMain() {
+    this.router.navigateByUrl('/main')
+  }
+}
