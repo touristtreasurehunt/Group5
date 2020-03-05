f3e639eda79ef8002ea11f6634e8458930578b9b
JoelLF <rlflaspalmas@hotmail.com>
2020-03-05 12:58:21 +0000

    Added leaflet

--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
-import { Router } from '@angular/router';;
+import { Router } from '@angular/router';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -8,6 +10,30 @@ import { Router } from '@angular/router';;
 })
 export class MapPage{
 
+  map: Map;
+
+  ionViewDidEnter() { this.leafletMap(); }
+
+  leafletMap() {
+    // In setView add latLng and zoom
+    this.map = new Map('mapId');
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '',
+    }).addTo(this.map);
+
+    this.map.setView([28.644800, 77.216721], 10);
+
+
+    // marker([28.6, 77]).addTo(this.map)
+    //   .bindPopup('Ionic 4 <br> Leaflet.')
+    //   .openPopup();
+  }
+
+  /** Remove map when we have multiple map object */
+  ionViewWillLeave() {
+    this.map.remove();
+  }
+  
   constructor(private router: Router) { }
 
   goToMap() {
