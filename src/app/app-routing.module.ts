76bd078fe52df397415c9c1831d378d697cb1865
JoelLF <rlflaspalmas@hotmail.com>
2020-03-11 13:23:13 +0000

    Deleted unused folders

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: '', loadChildren: './tab1/tab1.module#Tab1PageModule' },
   { path: 'map', loadChildren: './map/map.module#MapPageModule' },
-  { path: 'main', loadChildren: './main/main.module#MainPageModule' },
-  { path: 'map2', loadChildren: './map2/map2.module#Map2PageModule' },
-  { path: 'tab1', loadChildren: './tab1/tab1.module#Tab1PageModule' },
   { path: 'tutorial', loadChildren: './tutorial/tutorial.module#TutorialModule' },
   { path: 'modal', loadChildren: './modal/modal.module#ModalPageModule' },
 
